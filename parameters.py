434860c03e07516b3734729ed185f988aaeda040
Marco <marcoviero@gmail.com>
2017-01-26 17:00:56 -0800

    Up and running!

--- parameters.py
+++ parameters.py
@@ -27,6 +27,7 @@ def get_params(param_file_path):
     raw_params = dict(config.items('general'))
     raw_cosmo_params = dict(config.items('cosmology'))
     raw_io_params = dict(config.items('io'))
+    raw_binning_params = dict(config.items('binning'))
     raw_maps_to_stack_params = dict(config.items('maps_to_stack'))
     raw_map_path_params = dict(config.items('map_path'))
     raw_map_file_params = dict(config.items('map_file'))
@@ -43,15 +44,18 @@ def get_params(param_file_path):
     params['cosmo'] = get_cosmology_parameters(raw_cosmo_params)
     params['map_files'] = get_maps_parameters(raw_maps_to_stack_params,raw_map_path_params,raw_map_file_params)
     params['noise_files'] = get_maps_parameters(raw_maps_to_stack_params,raw_noise_path_params,raw_noise_file_params)
+    params['wavelength'] = get_wavelength_parameters(raw_maps_to_stack_params)
     params['psfs'] = get_beams_parameters(raw_maps_to_stack_params,raw_beams_params)
-    #params['cats'] = get_catalogs_parameters(raw_catalogs_params)
-    #params['bins'] = get_binning_parameters(raw_binning_params)
+    params['color_correction'] = get_color_correction_parameters(raw_maps_to_stack_params,raw_color_correction_params)
+    params['catalogs'] = get_catalogs_parameters(raw_catalogs_params)
+    params['bins'] = get_binning_parameters(raw_binning_params)
+    params['library_keys'] = params['map_files'].keys()
 
     logging.info("---------- PARAMETER VALUES ----------")
     logging.info("======================================")
     logging.info("\n" + pprint.pformat(params, indent=4) + "\n")
 
-    pdb.set_trace()
+    #pdb.set_trace()
     return params
 
 def get_general_params(raw_params):
@@ -64,42 +68,78 @@ def get_general_params(raw_params):
         params['galaxy_splitting_scheme'] = 'sf-qt'
 
     # If running bootstrap
-    try:
-        params['bootstrap'] = is_true(raw_params, 'bootstrap')
-    except KeyError:
+    if bool(raw_params['bootstrap'].split()[0]) == True:
+        params['bootstrap'] = True
+        params['boot0'] = float(raw_params['bootstrap'].split()[1])
+        params['number_of_boots'] = float(raw_params['bootstrap'].split()[2])
+    else:
         params['bootstrap'] = False
-    
-    # Number of bootstraps
-    if params['bootstrap'] == True:
-        params['boots'] = float(raw_params['boots'])
-
-    # If running bootstrap
-    try:
-        params['optimal_binning'] = is_true(raw_params, 'optimal_binning')
-    except KeyError:
-        params['optimal_binning'] = False
-    
-    # If running bootstrap
-    try:
-        params['bin_lookback_time'] = is_true(raw_params, 'bin_lookbackt')
-    except KeyError:
-        params['bin_lookback_time'] = False
-    
-    # If running bootstrap
-    try:
-        params['stack_all_z_at_once'] = is_true(raw_params, 'all_z_at_once')
-    except KeyError:
-        params['stack_all_z_at_once'] = False
-    
+   
     return params
+def get_wavelength_parameters(raw_maps_to_stack_params):
+    wavelengths = {}
+
+    for imap in raw_maps_to_stack_params:
+        if bool(raw_maps_to_stack_params[imap].split()[1]) == True:
+            wavelengths[imap] = float(raw_maps_to_stack_params[imap].split()[0])
+
+    return wavelengths
+
+def get_binning_parameters(raw_params):
+    binning = {}
+    # Style of binning, optimal or evenly, and the number of bins (optional).  
+    # If number_of_bins not provided, will be decided by the binning code.   
+    try:
+        binning['optimal_binning'] = raw_params['optimal_binning'].split()[0] 
+    except KeyError:
+        binning['optimal_binning'] = False
+
+    #Optional number of bins
+    if len(raw_params['optimal_binning'].split()) > 1:
+        try:
+            binning['number_of_bins'] = raw_params['optimal_binning'].split()[1]
+        except KeyError:
+            pass  
+    
+    # If binning in lookback time.  
+    # Should expand to bin in number densities in future... 
+    try:
+        binning['bin_in_lookback_time'] = is_true(raw_params, 'bin_in_lookbackt')
+    except KeyError:
+        binning['bin_in_lookback_time'] = False
+    
+    # If stacking entire catalog at once, rather that in redshift slices.  
+    # Still unclear if this is advantageous or not. 
+    try:
+        binning['stack_all_z_at_once'] = is_true(raw_params, 'all_z_at_once')
+    except KeyError:
+        binning['stack_all_z_at_once'] = False
+
+    if is_true(raw_params,'optimal_binning') == False:
+        z_nodes = []
+        m_nodes = []
+        for i in raw_params['redshift_nodes'].split():
+            z_nodes.append(float(i))
+        for j in raw_params['mass_nodes'].split():
+            m_nodes.append(float(j))
+
+        binning['z_nodes'] = z_nodes
+        binning['m_nodes'] = m_nodes
+
+    return binning
 
 def get_io_parameters(raw_params):
     io = {}
 
-    io['output_folder']           = raw_params['output_folder']
-    io['flux_densities_filename'] = raw_params['flux_densities_filename']
-    io['boot_fluxes_filename']    = raw_params['boot_fluxes_filename']
-    io['param_file_path']         = raw_params['param_file_path']
+    io['output_folder']              = raw_params['output_folder']
+    try:
+        io['output_bootstrap_folder']= raw_params['output_bootstrap_folder']
+    except KeyError:
+        io['output_bootstrap_folder']= raw_params['output_folder']
+
+    io['flux_densities_filename']    = raw_params['flux_densities_filename']
+    io['boot_fluxes_filename']       = raw_params['boot_fluxes_filename']
+    io['param_file_path']            = raw_params['param_file_path']
 
     return io
 
@@ -134,21 +174,28 @@ def get_beams_parameters(raw_maps_to_stack_params,raw_beams_params):
 
     for imap in raw_maps_to_stack_params:
         if bool(raw_maps_to_stack_params[imap].split()[1]) == True:
-            if is_float(raw_beams_params[imap]) == True:
-                psfs[imap+'_fwhm'] = float(raw_beams_params[imap])
+            psfs[imap+'_beam_area'] = float(raw_beams_params[imap].split()[1])
+            if is_float(raw_beams_params[imap].split()[0]) == True:
+                psfs[imap+'_fwhm'] = float(raw_beams_params[imap].split()[0])
             else:
-                psfs[imap+'_beam_file'] = raw_beams_params[imap] 
+                psfs[imap+'_beam_file'] = raw_beams_params[imap].split()[0]
 
-            pdb.set_trace()
     return psfs
 
-def get_color_correction_parameters(raw_color_correction_params):
+def get_color_correction_parameters(raw_maps_to_stack_params,raw_color_correction_params):
     color_correction = {}
+
+    for imap in raw_maps_to_stack_params:
+        if bool(raw_maps_to_stack_params[imap].split()[1]) == True:
+            color_correction[imap+''] = float(raw_color_correction_params[imap])
 
     return color_correction
 
-def get_color_catalog_parameters(raw_catalog_params):
+def get_catalogs_parameters(raw_catalog_params):
     catalog = {}
+
+    catalog['catalog_path'] = raw_catalog_params['catalog_path']
+    catalog['catalog_file'] = raw_catalog_params['catalog_file']
 
     return catalog
 
--- simstack_from_pfile.py
+++ simstack_from_pfile.py
@@ -8,10 +8,13 @@ import shutil
 import time
 import logging
 import importlib
+import pandas as pd
 from astropy.wcs import WCS
 
 # Modules within this package
 import parameters 
+from skymaps import Skymaps
+from skymaps import Field_catalogs
 from utils import circle_mask
 from utils import dist_idl
 from utils import gauss_kern
@@ -19,6 +22,7 @@ from utils import pad_and_smooth_psf
 from utils import shift_twod
 from utils import smooth_psf
 from lmfit import Parameters, minimize, fit_report
+from simstack import stack_libraries_in_layers
 
 def main():
 	# Set up logging
@@ -34,13 +38,13 @@ def main():
     t0 = time.time()
 
     # From parameter file read maps, psfs, cats, and divide them into bins
-    maps, psfs = get_maps(params)
-    cats       = get_cats(params)
-    bins       = get_bins(cats, params)
+    sky_library   = get_maps(params)
+    cats          = get_catalogs(params)
+    binned_ra_dec = get_bins(params, cats)
 
     # Do simultaneous stacking 
-
-    fluxes	   = do_simstack(maps, psfs, cats, bins)
+    pdb.set_trace()
+    stacked_flux_densities = stack_libraries_in_layers(sky_library,binned_ra_dec)
 
     # Summarize timing
     t1 = time.time()
@@ -50,14 +54,83 @@ def main():
     logging.info("")
     logging.info("Total time                        : {:.4f}\n".format(tpass))
 
-
-
 def get_maps(params):
-	'''
-	Read maps and psfs and store into dictionaries
-	'''
+    '''
+    Read maps and psfs and store into dictionaries
+    '''
+    sky_library = {}
+    
+    for t in params['library_keys']:
+        sky = Skymaps(params['map_files'][t],params['noise_files'][t],params['psfs'][t+'_fwhm'],color_correction=params['color_correction'][t])
+        sky.add_wavelength(params['wavelength'][t])
+        sky.add_fwhm(params['psfs'][t+'_fwhm']) 
+        #pdb.set_trace()
+        sky_library[t] = sky 
+    return sky_library 
 
-	return maps, psfs
+def get_catalogs(params):
+
+    # This formats the different catalogs to work with existing code.  
+    # It's not the most elegant solution; better would be to make the code work with different column names.  
+    tbl = pd.read_table(params['catalogs']['catalog_path']+params['catalogs']['catalog_file'],sep=',')
+
+    if 'ID' in tbl.keys():
+        pass
+    elif 'ALPHA_J2000' in tbl.keys():
+            tbl['ID']=range(len(tbl['ALPHA_J2000']))
+    elif 'id' in tbl.keys():
+            tbl['ID']=tbl['id']
+
+    if 'ra' in tbl.keys():
+        pass
+    elif 'ALPHA_J2000' in tbl.keys():
+        tbl['ra']=tbl['ALPHA_J2000']
+        tbl['dec']=tbl['DELTA_J2000']
+
+    if 'z_peak' in tbl.keys():
+        pass
+    elif 'ZPDF' in tbl.keys():
+        tbl['z_peak']=tbl['ZPDF']
+
+    if 'LMASS' in tbl.keys():
+        pass
+    elif 'MASS_MED' in tbl.keys():
+        tbl['LMASS']=tbl['MASS_MED']
+
+    if 'sfg' in tbl.keys():
+        pass
+    elif 'CLASS' in tbl.keys():
+        tbl['sfg']=tbl['CLASS']
+
+    return Field_catalogs(tbl)
+
+def get_bins(params, cats):
+
+    z_nodes = params['bins']['z_nodes']
+    m_nodes = params['bins']['m_nodes']
+    
+    cats.get_sf_qt_mass_redshift_bins(z_nodes,m_nodes)
+    binned_ra_dec = cats.subset_positions(cats.id_z_ms)
+
+    return binned_ra_dec
+
+def is_true(raw_params, key):
+    """Is raw_params[key] true? Returns boolean value.
+    """
+    sraw    = raw_params[key]
+    s       = sraw.lower() # Make case-insensitive
+
+    # Lists of acceptable 'True' and 'False' strings
+    true_strings    = ['true', 't', 'yes', 'y', '1']
+    false_strings    = ['false', 'f', 'no', 'n', '0']
+    if s in true_strings:
+        return True
+    elif s in false_strings:
+        return False
+    else:
+        logging.warning("Input not recognized for parameter: %s" % (key))
+        logging.warning("You provided: %s" % (sraw))
+        raise
 
 if __name__=="__main__":
     main()
